94d4bf4d5889f4b5599831c9668a4a3627cdb8df
markc <markc@pageuppeople.com>
2018-05-15 23:52:15 +1000

    Add dispatch variation

--- AutofacTests/MessageHandlerTests.cs
+++ AutofacTests/MessageHandlerTests.cs
@@ -1,10 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using Autofac;
 using FluentAssertions;
-using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Xunit;
 
 namespace AutofacTests
@@ -21,19 +19,19 @@ namespace AutofacTests
     {
     }
 
-    public interface MessageHandler<T> where T : IMessage
+    public interface IMessageHandler<in T> where T : IMessage
     {
         void Handle(T Message);
     }
 
-    public class MessageAHandler : MessageHandler<MessageA>
+    public class MessageAHandler : IMessageHandler<MessageA>
     {
         public void Handle(MessageA Message)
         {
         }
     }
 
-    public class MessageBHandler : MessageHandler<MessageB>
+    public class MessageBHandler : IMessageHandler<MessageB>
     {
         public void Handle(MessageB Message)
         {
@@ -48,7 +46,7 @@ namespace AutofacTests
 
             var builder = new ContainerBuilder();
             builder.RegisterAssemblyTypes(dataAccess)
-                .AsClosedTypesOf(typeof(MessageHandler<>))
+                .AsClosedTypesOf(typeof(IMessageHandler<>))
                 .AsImplementedInterfaces();
             return builder.Build();
         }
@@ -59,30 +57,55 @@ namespace AutofacTests
 
             IContainer container = CreateContainer();
 
-            var handler = container.Resolve<MessageHandler<MessageA>>();
+            var handler = container.Resolve<IMessageHandler<MessageA>>();
             
             handler.Should().BeOfType<MessageAHandler>();
         }
 
-        public MessageHandler<T> GetHandlerFor<T>(IContainer container, T m) where T : IMessage
+        public IMessageHandler<T> GetHandlerFor<T>(IContainer container, T m) where T : IMessage
         {
             Type t = m.GetType();
-            Type closedHandlerType = typeof(MessageHandler<>).MakeGenericType(t);
+            Type closedHandlerType = typeof(IMessageHandler<>).MakeGenericType(t);
             object handler = container.Resolve(closedHandlerType);
-            return (MessageHandler<T>)handler;
+            return (IMessageHandler<T>)handler;
         }
-        
+
         [Fact]
         public void CreateHandlerAUsingClosingTest()
         {
             IContainer container = CreateContainer();
 
             MessageA messageA = new MessageA();
-            MessageHandler<MessageA> handler = GetHandlerFor(container, messageA);
+            var handler = GetHandlerFor(container, messageA);
             
             handler.Should().BeOfType<MessageAHandler>();
             
             handler.Handle(messageA);
+        }
+
+        [Fact]
+        public void VarianceTest()
+        {
+            IEnumerable<string> ss = new List<string>(){"aa", "bb"};
+            IEnumerable<object> so = ss;
+        }
+        
+        
+        public void DispatchMessage<T>(IContainer c, T m) where T : IMessage
+        {
+            Type closedHandlerType = typeof(IMessageHandler<>).MakeGenericType(m.GetType());
+            object handler = c.Resolve(closedHandlerType);
+            MethodInfo handleMethod = closedHandlerType.GetMethod("Handle");
+            handleMethod.Invoke(handler, new object[] {m});
+        }
+
+        [Fact]
+        public void DispatchTest()
+        {
+            IContainer container = CreateContainer();
+
+            IMessage messageA = new MessageA();
+            DispatchMessage(container, messageA);
         }
     }
 }
