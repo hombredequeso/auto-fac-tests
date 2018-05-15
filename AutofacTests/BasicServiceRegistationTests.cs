ec6c295ec7ab0a686949405652d0ddd5853b2d89
markc <markc@pageuppeople.com>
2018-05-15 22:42:31 +1000

    Initial Commit

---
+++ AutofacTests/BasicServiceRegistationTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using Autofac;
+using FluentAssertions;
+using Xunit;
+
+namespace AutofacTests
+{
+    public interface IService
+    {
+    }
+
+    public class MyService : IService
+    {
+    }
+
+
+    public class BasicServiceRegistrationTests
+    {
+        public IContainer CreateExplicitContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MyService>().As<IService>();
+            return builder.Build();
+        }
+
+        [Fact]
+        public void ExplicitRegistration()
+        {
+            var container = CreateExplicitContainer();
+
+            var service = container.Resolve<IService>();
+            service.Should().BeOfType<MyService>();
+        }
+
+        public IContainer CreateScannedContainer()
+        {
+            var dataAccess = Assembly.GetExecutingAssembly();
+
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(dataAccess)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces();
+            return builder.Build();
+        }
+
+        [Fact]
+        public void ScannedRegistration()
+        {
+            var container = CreateScannedContainer();
+
+            var service = container.Resolve<IService>();
+            service.Should().BeOfType<MyService>();
+        }
+    }
+}
---
+++ AutofacTests/MessageHandlerTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Autofac;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Xunit;
+
+namespace AutofacTests
+{
+    public interface IMessage
+    {
+    }
+
+    public class MessageA : IMessage
+    {
+    }
+
+    public class MessageB : IMessage
+    {
+    }
+
+    public interface MessageHandler<T> where T : IMessage
+    {
+        void Handle(T Message);
+    }
+
+    public class MessageAHandler : MessageHandler<MessageA>
+    {
+        public void Handle(MessageA Message)
+        {
+        }
+    }
+
+    public class MessageBHandler : MessageHandler<MessageB>
+    {
+        public void Handle(MessageB Message)
+        {
+        }
+    }
+    
+    public class MessageHandlerTests
+    {
+        public IContainer CreateContainer()
+        {
+            var dataAccess = Assembly.GetExecutingAssembly();
+
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(dataAccess)
+                .AsClosedTypesOf(typeof(MessageHandler<>))
+                .AsImplementedInterfaces();
+            return builder.Build();
+        }
+
+        [Fact]
+        public void CreateHandlerATest()
+        {
+
+            IContainer container = CreateContainer();
+
+            var handler = container.Resolve<MessageHandler<MessageA>>();
+            
+            handler.Should().BeOfType<MessageAHandler>();
+        }
+
+        public MessageHandler<T> GetHandlerFor<T>(IContainer container, T m) where T : IMessage
+        {
+            Type t = m.GetType();
+            Type closedHandlerType = typeof(MessageHandler<>).MakeGenericType(t);
+            object handler = container.Resolve(closedHandlerType);
+            return (MessageHandler<T>)handler;
+        }
+        
+        [Fact]
+        public void CreateHandlerAUsingClosingTest()
+        {
+            IContainer container = CreateContainer();
+
+            MessageA messageA = new MessageA();
+            MessageHandler<MessageA> handler = GetHandlerFor(container, messageA);
+            
+            handler.Should().BeOfType<MessageAHandler>();
+            
+            handler.Handle(messageA);
+        }
+    }
+}
